fa67d30d8a73acd51b7032af7f124056b8fcaf8c
quinnjn <quinn@getbraintree.com>
2017-04-12 16:42:04 -0700

    Remove unused files

--- UITests/Helpers/BTUITest.swift
+++ UITests/Helpers/BTUITest.swift
@@ -2,7 +2,6 @@ import XCTest
 
 extension XCTestCase {
 
-#if swift(>=3.1)
     func waitForElementToAppear(_ element: XCUIElement, timeout: TimeInterval = 10,  file: String = #file, line: UInt = #line) {
         let existsPredicate = NSPredicate(format: "exists == true")
         
@@ -30,37 +29,9 @@ extension XCTestCase {
             }
         }
     }
-#else
-    func waitForElementToAppear(element: XCUIElement, timeout: NSTimeInterval = 10,  file: String = #file, line: UInt = #line) {
-        let existsPredicate = NSPredicate(format: "exists == true")
-
-        expectationForPredicate(existsPredicate, evaluatedWithObject: element, handler: nil)
-
-        waitForExpectationsWithTimeout(timeout) { (error) -> Void in
-            if (error != nil) {
-                let message = "Failed to find \(element) after \(timeout) seconds."
-                self.recordFailureWithDescription(message, inFile: file, atLine: line, expected: true)
-            }
-        }
-    }
-
-    func waitForElementToBeHittable(element: XCUIElement, timeout: NSTimeInterval = 10,  file: String = #file, line: UInt = #line) {
-        let existsPredicate = NSPredicate(format: "exists == true && hittable == true")
-
-        expectationForPredicate(existsPredicate, evaluatedWithObject: element, handler: nil)
-
-        waitForExpectationsWithTimeout(timeout) { (error) -> Void in
-            if (error != nil) {
-                let message = "Failed to find \(element) after \(timeout) seconds."
-                self.recordFailureWithDescription(message, inFile: file, atLine: line, expected: true)
-            }
-        }
-    }
-#endif
 }
 
 extension XCUIElement {
-#if swift(>=3.1)
     func forceTapElement() {
         if self.isHittable {
             self.tap()
@@ -69,14 +40,4 @@ extension XCUIElement {
             coordinate.tap()
         }
     }
-#else
-    func forceTapElement() {
-        if self.hittable {
-            self.tap()
-        } else {
-            let coordinate: XCUICoordinate = self.coordinateWithNormalizedOffset(CGVectorMake(0.0, 0.0))
-            coordinate.tap()
-        }
-    }
-#endif
 }

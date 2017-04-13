20a5e98b4ff056a773f97afa966c4cc58255cd32
quinnjn <quinn@getbraintree.com>
2017-04-13 13:12:22 -0700

    import BraintreeCore either locally or from the library

--- BraintreeVisaCheckout/Public/BraintreeVisaCheckout.h
+++ BraintreeVisaCheckout/Public/BraintreeVisaCheckout.h
@@ -6,7 +6,12 @@ FOUNDATION_EXPORT double BraintreeVisaCheckoutVersionNumber;
 //! Project version string for BraintreeVisaCheckout.
 FOUNDATION_EXPORT const unsigned char BraintreeVisaCheckoutVersionString[];
 
+#if __has_include("BraintreeCore.h")
 #import "BraintreeCore.h"
+#else
+#import <BraintreeCore/BraintreeCore.h>
+#endif
+
 #import "BTConfiguration+VisaCheckout.h"
 #import "BTVisaCheckoutAddress.h"
 #import "BTVisaCheckoutCardNonce.h"

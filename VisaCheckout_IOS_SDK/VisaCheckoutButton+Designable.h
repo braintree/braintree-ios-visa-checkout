945d47c988ddda300e34ad199e01e1073070237e
vhesener <vhesener@gmail.com>
2017-05-07 07:54:24 -0500

    Cleaned up file structure and updated to latest Visa Checkout SDK.

---
+++ VisaCheckout_IOS_SDK/VisaCheckoutButton+Designable.h
@@ -0,0 +1,6 @@
+/**
+ Copyright © 2017 Visa. All rights reserved.
+ */
+
+#import <UIKit/UIKit.h>
+@import VisaCheckoutSDK;
---
+++ VisaCheckout_IOS_SDK/VisaCheckoutButton+Designable.m
@@ -0,0 +1,10 @@
+/**
+ Copyright © 2017 Visa. All rights reserved.
+ */
+
+#import "VisaCheckoutButton+Designable.h"
+
+IB_DESIGNABLE
+@interface VisaCheckoutButton(Designable)
+@property (nonatomic) IBInspectable BOOL standardStyle;
+@end

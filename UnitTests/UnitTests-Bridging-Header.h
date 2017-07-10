e3c6ae4ece2d89a4cbeda7ae989608b29b333472
quinnjn <quinn@getbraintree.com>
2017-07-10 10:49:10 -0700

    Use Braintree module for UnitTests

--- UnitTests/UnitTests-Bridging-Header.h
+++ UnitTests/UnitTests-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "BraintreeCore.h"
+@import Braintree;
 #import "BTConfiguration+VisaCheckout.h"
 #import "BTVisaCheckoutAddress.h"
 #import "BTVisaCheckoutCardNonce.h"

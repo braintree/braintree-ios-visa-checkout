1c42401efa26647fdb8c79143f910ab5fa90eedd
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-11-22 15:01:02 -0600

    Add unnecessary SPM file to gitignore

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BraintreeVisaCheckout",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "BraintreeVisaCheckout",
+            targets: ["BraintreeVisaCheckout", "VisaCheckoutSDK"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/braintree/braintree_ios", .upToNextMajor("5.5.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "BraintreeVisaCheckout",
+            dependencies: [
+                .product(name: "BraintreeCore", package: "Braintree")
+            ],
+            exclude: ["Info.plist"],
+            publicHeadersPath: "Public"
+        ),
+        .binaryTarget(
+            name: "VisaCheckoutSDK",
+            path: "Frameworks/VisaCheckoutSDK.xcframework"
+        ),
+    ]
+)

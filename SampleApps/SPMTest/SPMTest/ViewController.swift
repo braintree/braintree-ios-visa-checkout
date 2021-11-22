626325b0a8f278dfe226622c35b083b1bd49e93a
Samantha Cannillo <samantha.cannillo@getbraintree.com>
2021-11-22 15:18:58 -0600

    Add SPMTest app

--- SampleApps/SPMTest/SPMTest/ViewController.swift
+++ SampleApps/SPMTest/SPMTest/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  SPMTest
-//
-//  Created by Samantha Cannillo on 11/22/21.
-//
-
 import UIKit
+import BraintreeVisaCheckout
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
